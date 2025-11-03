4919ee766e7a9431a8c2a1646517b35ec0602574
phamthainb <phamthai4643@gmail.com>
2025-11-04 01:28:32 +0700

    Refactor configuration and page components; standardize code formatting and enhance loading experience with Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-'use server';
+"use server";
 import { IdeaList } from "@/components/idea-list";
+import { Suspense } from "react";
 
 export default async function Home() {
-  return <IdeaList initialIdeas={[]} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <IdeaList initialIdeas={[]} />;
+    </Suspense>
+  );
 }
